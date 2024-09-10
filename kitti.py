bb7fb3247c0c58fcd57e557ea7170c77ddff4ad4
woosoojin <wsj1998@naver.com>
2024-09-11 02:13:02 +0900

    update kittiLseg

--- kitti.py
+++ kitti.py
@@ -32,6 +32,137 @@ def get_kitti_dataset(random_dataset): # TODO
     return KittiDatasetNetVLAD(image_path, gt_path, random_dataset,
                              input_transform=input_transform())
 
+def get_kitti_dataset_lseg(): # TODO
+    image_path = join(root_dir, 'kitti/00/image_2') # TODO
+    gt_path = join(root_dir, 'kitti/00') # TODO
+    return KittiDatasetLseg(image_path, gt_path)
+
+class KittiDatasetLseg(data.Dataset):
+    def __init__(self, image_path, gt_path, input_transform=None, onlyDB=False): # TODO
+        super().__init__()
+
+        self.input_transform = input_transform
+
+        self.whole_image = sorted(os.listdir(image_path))
+        self.images = [join(image_path, dbIm) for dbIm in self.whole_image]
+        
+        # ground truth
+        self.gt_pose_path = join(gt_path, 'poses.txt')
+        with open(self.gt_pose_path, 'r') as poses:
+            self.utm_coord = [[float(pose.split()[3]), float(pose.split()[7])] for pose in poses]
+
+        # if not onlyDB: # TODO
+        #     self.images += [join(queries_dir, qIm) for qIm in self.dbStruct.qImage]
+
+        # self.whichSet = self.dbStruct.whichSet
+        # self.dataset = self.dbStruct.dataset
+
+        self.positives = None
+        # self.distances = None
+        self.posDistThr = 25
+
+        self.numDb = int(len(self.images)/2) # TODO
+        self.numQ = len(self.images) - self.numDb
+
+        # if self.random_dataset:
+        print('===> Randomizing kitti dataset')
+        self.total_dataset = len(self.images)
+        self.randIdx = random.sample(range(self.total_dataset), self.total_dataset)
+
+        self.images = [self.images[i] for i in self.randIdx]
+        self.utm_coord = [self.utm_coord[i] for i in self.randIdx]
+
+    def __getitem__(self, index):
+        bgr = cv2.imread(self.images[index])
+        img = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+
+        return img, index
+
+    def __len__(self):
+        return len(self.images)
+    
+    def calculate_distance_diff(self, q_pose, gt_pose):
+        qx = float(q_pose[0])
+        qy = float(q_pose[1])
+        qz = float(q_pose[2])
+
+        gt_x = float(gt_pose[0])
+        gt_y = float(gt_pose[1])
+        gt_z = float(gt_pose[2])
+
+        return math.sqrt((gt_x - qx)**2 + (gt_y - qy)**2 + (gt_z - qz)**2)
+
+    def get_positives(self): # TODO
+        # positive = list()
+        # q_pose = self.gt_poses[idx]
+        
+        # for gt_idx, gt_pose in enumerate(self.gt_poses):
+        #     diff = self.calculate_distance_diff(q_pose, gt_pose)
+
+        #     if(diff <= thres_distance):
+        #         positive.append(gt_idx)
+
+        # return np.array(positive)
+
+        if  self.positives is None:
+            knn = NearestNeighbors(n_jobs=-1)
+            self.utm_coord = np.array(self.utm_coord)
+            knn.fit(self.utm_coord[:self.numDb])
+
+            self.distances, self.positives = knn.radius_neighbors(self.utm_coord[self.numDb:],
+                    radius=self.posDistThr)
+        return self.positives
+    
+    def build_codebook(self, X, voc_size=10): # TODO voc_size 10
+        n, desc, dim = X.shape
+        X = X.reshape(n*desc, dim)
+        
+        kmeans = KMeans(n_clusters= voc_size, n_init=10)
+        kmeans.fit(X)
+        pred = kmeans.cluster_centers_
+        
+        return pred
+    
+    def bag_of_words(self, descriptor, codebook):
+        descriptor=descriptor.tolist()
+        index = vq.vq(descriptor,codebook)
+        hist, _ = np.histogram(index,bins=range(codebook.shape[0] + 1), density=True)
+        
+        return hist
+    
+    def calculate_recall(self, dbFeat, encoder_dim=10):
+        qFeat = dbFeat[self.numDb:].astype('float32') # TODO
+        dbFeat = dbFeat[:self.numDb].astype('float32')
+        
+        print('====> Building faiss index')
+        faiss_index = faiss.IndexFlatL2(encoder_dim)
+        faiss_index.add(dbFeat)
+
+        print('====> Calculating recall @ N')
+        n_values = [1,5,10,20]
+
+        _, predictions = faiss_index.search(qFeat, max(n_values))
+
+        # for each query get those within threshold distance
+        gt = self.get_positives() 
+
+        correct_at_n = np.zeros(len(n_values))
+        for qIx, pred in enumerate(predictions):
+            for i,n in enumerate(n_values):
+                if np.any(np.in1d(pred[:n], gt[qIx])):
+                    correct_at_n[i:] += 1
+                    break
+                
+        recall_at_n = correct_at_n / self.numQ
+        print(recall_at_n)
+
+        recalls = {} #make dict for output # TODO
+        for i,n in enumerate(n_values):
+            recalls[n] = recall_at_n[i]
+            print("====> Recall@{}: {:.4f}".format(n, recall_at_n[i]))
+
+        return recalls
+
 class KittiDatasetNetVLAD(data.Dataset):
     def __init__(self, image_path, gt_path, random_dataset, input_transform=None, onlyDB=False): # TODO
         super().__init__()
@@ -63,7 +194,7 @@ class KittiDatasetNetVLAD(data.Dataset):
         if self.random_dataset:
             print('===> Randomizing kitti dataset')
             random.seed(time.time())
-            
+
             self.total_dataset = len(self.images)
             self.randIdx = random.sample(range(self.total_dataset), self.total_dataset)
 
