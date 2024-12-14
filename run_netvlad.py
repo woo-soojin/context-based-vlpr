6ba910d165c71deefeb34a8846a8e8a8f6b44c69
woo-soojin <soojin.woo@snu.ac.kr>
2024-12-14 18:54:38 +0900

    fix error

--- run_netvlad.py
+++ run_netvlad.py
@@ -71,7 +71,7 @@ parser.add_argument('--margin', type=float, default=0.1, help='Margin for triple
 parser.add_argument('--split', type=str, default='val', help='Data split to use for testing. Default is val', 
         choices=['test', 'test250k', 'train', 'val'])
 parser.add_argument('--fromscratch', action='store_true', help='Train from scratch rather than using pretrained models')
-parser.add_argument('--random', type=bool, default=False, help='Randomize dataset for test')
+parser.add_argument('--random_dataset', type=bool, default=False, help='Randomize dataset for test')
 parser.add_argument('--extract_dataset', type=bool, default=False, help='Extract partial dataset from whole dataset') # TODO
 
 def train(epoch):
@@ -426,7 +426,7 @@ if __name__ == "__main__":
         train_set = dataset.get_training_query_set(opt.margin)
 
         print('====> Training query set:', len(train_set))
-        whole_test_set = dataset.get_whole_val_set(opt.extract_dataset, opt.random)
+        whole_test_set = dataset.get_whole_val_set(opt.extract_dataset, opt.random_dataset)
         print('===> Evaluating on val set, query count:', whole_test_set.dbStruct.numQ)
     elif opt.mode.lower() == 'test':
         if opt.split.lower() == 'test':
@@ -440,10 +440,10 @@ if __name__ == "__main__":
             print('===> Evaluating on train set')
         elif opt.split.lower() == 'val':
             if opt.dataset.lower() == 'pittsburgh':
-                whole_test_set = dataset.get_whole_val_set(opt.extract_dataset, opt.random)
+                whole_test_set = dataset.get_whole_val_set(opt.extract_dataset, opt.random_dataset)
                 print('===> Evaluating on val set')
             elif opt.dataset.lower() == 'kitti': # TODO
-                whole_test_set = dataset.get_kitti_dataset(opt.random)
+                whole_test_set = dataset.get_kitti_dataset(opt.random_dataset)
                 print('===> Evaluating on kitti dataset')
         else:
             raise ValueError('Unknown dataset split: ' + opt.split)
